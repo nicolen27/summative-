2c67d1759cef7bc08e53688078884b6b24a787fd
nicolen27 <n.nwanze@uea.ac.uk>
2021-05-12 10:35:56 +0000

    testing

--- summative -.R
+++ summative -.R
@@ -1,11 +1,23 @@
+# load packages
+
 library(knitr)
 library(ggplot2)
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
 
-
+# read in csv file
 Ladybird <- read.csv("/cloud/project/Ladybird.csv")
-head(Ladybird)
 
-ggplot(Ladybird, aes(Avg_area,Mass.Ave, color=Sex))+ geom_boxplot()
+# print the first lines of dataframe
+print(head(Ladybird))
+
+
+# describe the number of different femles males, colour, distribution of weigths ect.
+
+
+# ggplot + .... bar plot with subgroups ect 
+
 
 # princple component analysis
 ladybirdsubset <-Ladybird[ ,c(3,5:16)]
@@ -13,3 +25,28 @@ ladybirdsubsetnona <- na.omit(Ladybird[ ,c(3,5:16)])
 ladybird.pca <- prcomp(ladybirdsubsetnona, center=TRUE, scale=TRUE)
 
 summary(ladybird.pca)
+
+
+# seperate out ladybird weights w/ melanic and typical
+weight_melanic <-  Ladybird$Mass.Ave[Ladybird$Colour.morph=="M"]
+weight_typical <-  Ladybird$Mass.Ave[Ladybird$Colour.morph=="T"]
+
+# create an unpaired ttest for loading into boxplot
+test <- t.test(weight_melanic, weight_typical, var.equal=TRUE) %>% adjust_pvalue(method="bonferroni") %>% add_significance()
+
+print(test)
+
+ggplot(Ladybird, aes(Colour.morph,Mass.Ave))+ geom_boxplot()+labs(x= 'Colour Morph', y= 'Average Mass') +
+  scale_x_discrete(labels=c('Melanic','Typical')) + stat_compare_means(method="t.test") 
+
+
+# males distribution vs feamles for the tuck time
+
+# aphid attack- melanic 
+
+ggplot(Ladybird, aes(Sex,P.time.mean)) + geom_point()+labs(x= 'Sex', y= 'Time took to attack aphid')
+
+# changing 
+
+
+
